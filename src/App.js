db73ddc151a64cbf09b167f7001741a5cfdafb99
mammamon <quangnguyenonlinevn@gmail.com>
2023-08-02 00:33:17 +0700

    base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import ShoeStore from './ShoeStore/ShoeStore'
+function App() {
+    return (
+        // <div></div>
+        <div className="App">
+            <ShoeStore />
+        </div>
+    )
+}
+
+export default App
